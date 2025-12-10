dd4db919792ac85f3393504eca59ec50b57aa875
astrNVlik <wertybrothers9@gmail.com>
2025-12-10 15:44:08 +0700

    commit for day3 tugas

---
+++ e-commerce-api/src/services/product.service.ts
@@ -0,0 +1,62 @@
+import  prisma  from '../prisma';
+import { Product } from ".generated/Prisma"
+
+export const getAllProducts = async (): Promise<Product[]> => {
+  return await prisma.product.findMany();
+};
+
+export const getProductById = async (id: number): Promise<Product> => {
+  const product = await prisma.product.findUnique({
+    where: { id },
+  });
+  
+  if (!product) {
+    throw new Error('Product not found');
+  }
+  
+  return product;
+};
+
+export const createProduct = async (data: { 
+  name: string; 
+  price: number; 
+  stock: number;
+  description?: string; 
+}): Promise<Product> => {
+  return await prisma.product.create({
+    data: {
+      name: data.name,
+      description: data.description ?? null,
+      price: data.price,
+      stock: data.stock,
+    },
+  });
+};
+
+export const updateProduct = async (id: number, data: Partial<Product>): Promise<Product> => {
+  await getProductById(id); // Cek existance
+
+  return await prisma.product.update({
+    where: { id },
+    data,
+  });
+};
+
+export const deleteProduct = async (id: number): Promise<Product> => {
+  await getProductById(id); // Cek existance
+
+  return await prisma.product.delete({
+    where: { id },
+  });
+};
+
+export const searchProducts = async (name?: string, maxPrice?: number): Promise<Product[]> => {
+  let result = await getAllProducts();
+  if (name) {
+    result = result.filter(p => p.name.toLowerCase().includes(name.toLowerCase()));
+  }
+  if (maxPrice) {
+    result = result.filter(p => p.price.toNumber() <= maxPrice);
+  }
+  return result;
+};
